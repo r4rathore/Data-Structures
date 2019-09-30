1fb98ba3b60a24ccbc9fa7c4045fb82e7b14aacb
rahul <rathorerahul2727@gmail.com>
2019-09-30 19:48:21 +0530

    --Tree Operations

--- Tree/BinarySearchTree.py
+++ Tree/BinarySearchTree.py
@@ -23,9 +23,47 @@ class BST:
     def insert(self, data):
         self.root = BST.insert_helper(self.root, data)
 
+    def min(self):
+        current:Node = self.root
+        if current is None:
+            return None
+        while current.left is not None:
+            current = current.left
+
+        return current.data
+
+    def max(self):
+        current:Node = self.root
+        if current is None:
+            return None
+        while current.right is not None:
+            current = current.right
+
+        return current.data
+
+    def hight_of_tree(root:Node):
+        if root is None:
+            return -1
+
+        left_tree = BST.hight_of_tree(root.left)+1
+        right_tree = BST.hight_of_tree(root.right) +1
+
+        return BST.maxValue(left_tree, right_tree)
+
+    def maxValue(leftnodedata, rightnodedata):
+        if leftnodedata >= rightnodedata:
+            return leftnodedata
+        else: return rightnodedata
+
+    def tree_Heigh(self):
+        if self.root is None:
+            return 0
+        return BST.hight_of_tree(self.root)
+
+
 tree = BST()
 tree.insert(4)
 tree.insert(5)
 tree.insert(1)
 tree.insert(2)
-
+print(tree.tree_Heigh())
